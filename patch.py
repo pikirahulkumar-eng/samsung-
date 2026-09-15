7d6a370124512a74800a13b2fae2ff5c26a5a4d4
merahulrj <merahulrj@gmail.com>
2026-09-15 22:31:58 +0530

    Bypass ConnectionState and showNoCompatibleDialog

--- patch.py
+++ patch.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 import sys
 
 NL = chr(10)
@@ -12,8 +12,9 @@ def patch_smali_methods(content):
     is_root_dialog = 'RootingDetectedDialog' in content
     is_setup = 'SHealthMonitorSetupActivity' in content
     is_restriction = any(k in content for k in ['RestrictionView', 'isSupportedCountry', 'isSupportCountry', 'common_restriction', 'CommonConstants$SupportedType'])
+    is_conn = any(k in content for k in ['ConnectionState', 'BaseEcgOnboardingCard', 'NoCompatibleWatchCard', 'showNoCompatibleDialog'])
 
-    if not (is_root_util or is_validator or is_root_dialog or is_setup or is_restriction):
+    if not (is_root_util or is_validator or is_root_dialog or is_setup or is_restriction or is_conn):
         return content
 
     methods = content.split('\n.method ')
@@ -56,15 +57,22 @@ def patch_smali_methods(content):
         elif any(k in header for k in ['getNetworkCountryIso', 'getSimCountryIso', 'getCountryCode']) and header.strip().endswith(')Ljava/lang/String;'):
             new_m = header + NL + regs + NL + '    const-string v0, "US"' + NL + '    return-object v0' + NL + '.end method'
             res.append(new_m)
-        # 6. SupportedType methods: always return SUPPORTED (d)
+        # 6. SupportedType methods: always return SUPPORTED (a)
         elif header.strip().endswith(')Lcom/samsung/android/shealthmonitor/util/CommonConstants$SupportedType;'):
             new_m = header + NL + regs + NL + '    sget-object v0, Lcom/samsung/android/shealthmonitor/util/CommonConstants$SupportedType;->a:Lcom/samsung/android/shealthmonitor/util/CommonConstants$SupportedType;' + NL + '    return-object v0' + NL + '.end method'
             res.append(new_m)
-        # 7. Disable showErrorDialog specifically in SetupActivity
-        elif is_setup and 'showErrorDialog(' in header:
-            new_m = header + NL + regs + NL + '    return-void' + NL + '.end method'
+        # 7. ConnectionState methods: always return CONNECTED (b)
+        elif header.strip().endswith(')Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;'):
+            new_m = header + NL + regs + NL + '    sget-object v0, Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;->b:Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;' + NL + '    return-object v0' + NL + '.end method'
             res.append(new_m)
-        # 8. Disable RootingDetectedDialog
+        # 8. Disable showErrorDialog & showNoCompatibleDialog
+        elif (is_setup and 'showErrorDialog(' in header) or 'showNoCompatibleDialog' in header:
+            if header.strip().endswith(')V'):
+                new_m = header + NL + regs + NL + '    return-void' + NL + '.end method'
+                res.append(new_m)
+            else:
+                res.append(m)
+        # 9. Disable RootingDetectedDialog
         elif is_root_dialog:
             if 'onCreateDialog' in header:
                 new_m = header + NL + regs + NL + '    const/4 v0, 0x0' + NL + '    return-object v0' + NL + '.end method'
@@ -102,7 +110,16 @@ def patch_file(filepath):
                 lines[i] = f'    const-string {reg}, "samsung"'
         content = NL.join(lines)
 
-    # 2. Neutralize root binary, test-keys, and su strings
+    # 2. Force ConnectionState field read to always be CONNECTED (b)
+    if "Lwf/g;->b:Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;" in content:
+        lines = content.splitlines()
+        for i, line in enumerate(lines):
+            if "iget-object" in line and "Lwf/g;->b:Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;" in line:
+                reg = line.strip().split()[1].replace(',', '')
+                lines[i] = f'    sget-object {reg}, Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;->b:Lcom/samsung/android/shealthmonitor/wearable/v2/common/ConnectionState;'
+        content = NL.join(lines)
+
+    # 3. Neutralize root binary, test-keys, and su strings
     content = content.replace('"test-keys"', '"release-keys"')
     content = content.replace('"/system/bin/su"', '"/system/bin/no_su"')
     content = content.replace('"/system/xbin/su"', '"/system/xbin/no_su"')
@@ -110,7 +127,7 @@ def patch_file(filepath):
     content = content.replace('"/system/app/Superuser.apk"', '"/system/app/none"')
     content = content.replace('"su"', '"no_su"')
 
-    # 3. Clean method body replacement
+    # 4. Clean method body replacement
     content = patch_smali_methods(content)
 
     if content != original:
